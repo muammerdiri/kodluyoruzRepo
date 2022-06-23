60e22a44971db6c434d048fddea6af974235b2f3
Muammer Diri <muammerdiri2@gmail.com>
2022-06-23 20:22:47 +0300

    Lesson 5 added

--- SQL/odev5.sql
+++ SQL/odev5.sql
@@ -2,6 +2,7 @@
 select title,length from film where title like '%n' order by length desc limit 5;
 
 -- film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
-select title,length from film where title like '%n' order by length offset 5 limit 5
+select title,length from film where title like '%n' order by length offset 5 limit 5;
 
 -- customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+select last_name from customer where store_id ='1' order by last_name desc limit 5;
